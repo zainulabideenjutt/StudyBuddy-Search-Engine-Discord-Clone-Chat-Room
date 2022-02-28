53e278899126d47e72d7c5ba86386dcc81033e2a
Asad Raza <asad.9800545@talmeez.pk>
2022-02-28 20:29:38 +0500

    initial commit

---
+++ base/forms.py
@@ -0,0 +1,8 @@
+from .models import Room
+from django.forms import ModelForm
+
+class RoomForm(ModelForm):
+    class Meta :
+        model=Room
+        fields='__all__'
+        exclude=['host','participants']
